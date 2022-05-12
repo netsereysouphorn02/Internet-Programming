628c3dad1dc52946f8401679c7cf5629cf2ecc9e
netsereysouphorn02 <souphorn02@gmail.com>
2022-05-12 22:15:30 +0700

    first commit

---
+++ ex2/src/components/ProductCard.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="card">
+    <img
+      class="card-img"
+      :src="product.imageUrl"
+      alt=""
+    />
+    <div class="card-title">{{ product.name }}</div>
+    <br />
+    <div class="card-price" v-for="p in product.prices" :key="p._id">{{ p.source }} - {{ p.price }}$</div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: ["product"]
+};
+</script>
+
+<style scoped>
+.card {
+  width: 160px;
+  height: 220px;
+  border: 1px solid;
+}
+
+.card-img {
+  width: 100%;
+}
+
+.card-title {
+  text-align: center;
+}
+
+.card-price {
+  color: red;
+  font-weight: bold;
+  text-align: center;
+}
+</style>
